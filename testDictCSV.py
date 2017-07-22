1a520fa814167d34e14b401840bcac1dea6a17cc
NewConn <n1141359233@gmail.com>
2017-07-22 13:20:54 +0800

    optimization
    优化

--- testDictCSV.py
+++ testDictCSV.py
@@ -1,13 +1,33 @@
 import itchat
 import csv
-
+import codecs
 itchat.login()
 
 friends = itchat.get_friends(update=True)[0:]
 total = len(friends[1:])
-with open('names.csv', 'wb') as csvfile:
-    fieldnames = ['Sex', 'NickName', 'Province', 'City', 'Signature']
-    dict_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    dict_writer.writerow(fieldnames)
-    dict_writer.writerows(friends)
 
+# fieldnames = ['NickName', 'Sex', 'Province', 'City', 'Signature']
+class friend():
+    def __init__(self, fri):
+        self.csvFrame = {}
+        self.csvFrame['NickName'] = fri['NickName']
+        sex = fri['Sex']
+        if sex == 1:
+            self.csvFrame['Sex'] = '男'
+        elif sex == 2:
+            self.csvFrame['Sex'] = '女'
+        else:
+            self.csvFrame['Sex'] = '未知'
+
+        self.csvFrame['Province'] = fri['Province']
+        self.csvFrame['City'] = fri['City']
+        self.csvFrame['Signature'] = fri['Signature']
+
+file = codecs.open('names.csv', 'w', 'gbk', 'ignore')
+fieldnames = ['NickName', 'Sex', 'Province', 'City', 'Signature']
+writer = csv.DictWriter(file, fieldnames=fieldnames)
+writer.writeheader()
+for fri in friends:
+    member = friend(fri)
+    writer.writerow(member.csvFrame)
+file.close()
